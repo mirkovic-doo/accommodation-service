9caf4b269a3979d4e5baa9e853f82e6e3c9acb3a
Bane Geric <bane.geric@servalit.com>
2024-08-20 23:45:23 +0200

    Feature 4: Added CRUD for Property entity

--- AccommodationService/Application/Services/IPropertyService.cs
+++ AccommodationService/Application/Services/IPropertyService.cs
@@ -5,4 +5,10 @@ namespace AccommodationService.Application.Services;
 public interface IPropertyService
 {
     Task<Property> CreateAsync(Property property);
+
+    Task<Property> GetAsync(Guid id);
+
+    void Delete(Guid id);
+
+    Property Update(Property property);
 }
