3adcd47ae05e00354ea958512819c0ae7e20b150
Bane Geric <bane.geric@servalit.com>
2024-08-20 23:45:23 +0200

    Feature 4: Added CRUD for Property entity

---
+++ AccommodationService/Migrations/20240820094008_ProperyNewAttributes.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccommodationService.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProperyNewAttributes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string[]>(
+                name: "Amenities",
+                table: "Properties",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new string[0]);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Properties",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxGuests",
+                table: "Properties",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MinGuests",
+                table: "Properties",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string[]>(
+                name: "Photos",
+                table: "Properties",
+                type: "text[]",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amenities",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "MaxGuests",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "MinGuests",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "Photos",
+                table: "Properties");
+        }
+    }
+}
