abd3ab2eae9b0e2b59c046e3b726ea02c31885a2
Bane Geric <bane.geric@servalit.com>
2024-08-21 23:12:10 +0200

    Add Fluent validator for ProperyRequest

---
+++ AccommodationService/Infrastructure/Validators/PropertyValidator.cs
@@ -0,0 +1,14 @@
+﻿using AccommodationService.Controllers.Property.Requests;
+using FluentValidation;
+
+namespace AccommodationService.Infrastructure.Validators;
+
+public class PropertyValidator : AbstractValidator<PropertyRequest>
+{
+    public PropertyValidator()
+    {
+        RuleFor(x => x.MinGuests).GreaterThan(0).NotEmpty();
+        RuleFor(x => x.MaxGuests).GreaterThan(0).NotEmpty();
+        RuleFor(x => x).Must(x => x.MinGuests <= x.MaxGuests).WithMessage("MinGuests must be less than or equal to MaxGuests.");
+    }
+}
