f2c8640dbbca8b2736a8c93c16008da6bd730624
Dragan Mirkovic <63748840+Dragan2402@users.noreply.github.com>
2024-08-28 19:59:21 +0200

    Feature 22: Notification sending (#23)
    * Feature 20: Added delete action cases
    
    For guest we check for active reservations if there are not any we delete all of his reservations.
    For host we check if his properties have any upcoming reservation, if there are not we delete all of his properties and reservations.
    
    * Feature 20: Added reservation request validator
    
    * Feature 22: Notification sending when reservation status changes
    
    ---------
    
    Co-authored-by: banzg00 <63750104+banzg00@users.noreply.github.com>

--- AccommodationService/Infrastructure/Repositories/ReservationRepository.cs
+++ AccommodationService/Infrastructure/Repositories/ReservationRepository.cs
@@ -14,6 +14,18 @@ public class ReservationRepository : BaseRepository<Reservation>, IBaseRepositor
         this.dbContext = dbContext;
     }
 
+    public override async Task<Reservation> GetAsync(Guid id)
+    {
+        var entity = await dbContext.Reservations.Include(r => r.Property).SingleOrDefaultAsync(e => e.Id == id);
+
+        if (entity == null)
+        {
+            throw new Exception($"Entity with {id} not found");
+        }
+
+        return entity;
+    }
+
     public async Task DeleteGuestReservationsAsync(Guid guestId)
     {
         await dbContext.Reservations
@@ -29,15 +41,19 @@ public class ReservationRepository : BaseRepository<Reservation>, IBaseRepositor
     public async Task<IEnumerable<Reservation>> GetAllPendingCorrelated(Reservation reservation)
     {
         return await dbContext.Reservations
+            .Include(r => r.Property)
             .Where(r => r.Status == ReservationStatus.Pending &&
-            (r.StartDate <= reservation.StartDate && r.EndDate >= reservation.StartDate) ||
-            (r.StartDate <= reservation.EndDate && r.EndDate >= reservation.EndDate))
+            r.Id != reservation.Id &&
+            r.PropertyId == reservation.PropertyId &&
+            ((r.StartDate <= reservation.StartDate && r.EndDate >= reservation.StartDate) ||
+            (r.StartDate <= reservation.EndDate && r.EndDate >= reservation.EndDate)))
             .ToListAsync();
     }
 
     public async Task<IEnumerable<Reservation>> GetGuestReservationsAsync(Guid guestId)
     {
         return await dbContext.Reservations
+            .Include(r => r.Property)
             .Where(r => r.CreatedById == guestId &&
             (r.Status == ReservationStatus.Pending || r.Status == ReservationStatus.Confirmed))
             .ToListAsync();
